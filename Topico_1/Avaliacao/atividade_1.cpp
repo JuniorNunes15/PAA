56a3b3e2e11b2ab5d4f515b4b31fc25874415754
JuniorNunes15 <juniornunesufc@gmail.com>
2023-04-16 22:28:45 -0300

    atividade 01

--- Topico_1/Avaliacao/atividade_1.cpp
+++ Topico_1/Avaliacao/atividade_1.cpp
@@ -23,15 +23,30 @@ void ordenacao_selecao(int n, int v[]) {
     }
 }
 
+void ordenacao_selecao2(int n, int v[]) {
+    for(int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for(int j = i+1; j < n; j++) {
+            if(v[j] < v[min_idx]) {
+                min_idx = j;
+                if(min_idx != i) 
+                    swap(v[i],v[min_idx]);
+            }
+                
+            
+        }
+    }
+}
+
 int main() {
 
     int x;
-    cin >> x;
+    //cin >> x;
 
-    cout << calcula_raiz(x);
+    //cout << calcula_raiz(x);
 
     int v[8] = {5,7,6,4,3,7,2,10};
-    ordenacao_selecao(8, v);
+    ordenacao_selecao2(8, v);
 
     for(int i = 0; i < 8; i++) {
         cout << v[i] << " ";
